c9cbd8e7814050916a91a5dea3f948f5a37af00f
Keen Regen <keenregen@gmail.com>
2023-04-12 07:05:09 +0300

    v2.0

--- contracts/MrkplcResale.sol
+++ contracts/MrkplcResale.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BLANK
 
 // pragma
-pragma solidity 0.8.9;
+pragma solidity 0.8.15;
 
 // imports
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
@@ -18,13 +18,15 @@ error MrkplcResale__NotHolder();
 
 // Contracts
 
+// Contract Address: 0xe4B139bE66f03ac66C882F54592f01cf0edE2af7
+
 /// @title A Contract For Nft Resale
 /// @author keenregen
 /// @notice A mrkplc contract for study purposes
 /// @custom:experimental This is an experimental contract.
 contract MrkplcResale is IERC721Receiver, ReentrancyGuard, Ownable {
     address payable holder;
-    uint256 listingFee = 0.00001 ether;
+    uint256 listingFee = 0.000001 ether;
 
     struct List {
         uint256 tokenId;
@@ -34,9 +36,9 @@ contract MrkplcResale is IERC721Receiver, ReentrancyGuard, Ownable {
         bool sold;
     }
 
-    mapping(uint256 => List) public vaultItems;
+    mapping(uint256 => List) public fairItems;
 
-    event NFTListGenerated(
+    event NftListed(
         uint256 indexed tokenId,
         address seller,
         address holder,
@@ -49,9 +51,9 @@ contract MrkplcResale is IERC721Receiver, ReentrancyGuard, Ownable {
     // Modifiers
 
     modifier onlyHolder() {
-        // require(msg.sender == i_holder, "Sender must be contract holder.");
+        // require(msg.sender == holder, "Sender must be contract holder.");
         // gas efficient way for errors
-        if (msg.sender != i_holder) revert MrkplcResale__NotHolder();
+        if (msg.sender != holder) revert MrkplcResale__NotHolder();
         _;
     }
 
@@ -60,7 +62,81 @@ contract MrkplcResale is IERC721Receiver, ReentrancyGuard, Ownable {
     // called when the contract is deployed
     constructor(ERC721Enumerable _nft) {
         holder = payable(msg.sender);
-        nft = _nft
+        nft = _nft;
+    }
+
+    function withdraw() public payable onlyHolder {
+        require(payable(msg.sender).send(address(this).balance));
+    }
+
+    function listSale(
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(nft.ownerOf(tokenId) == msg.sender, "Nft is not yours");
+        require(fairItems[tokenId].tokenId == 0, "Nft was already listed");
+        require(price > 0, "Amount must be higher than 0");
+        require(
+            msg.value == listingFee,
+            "Please transfer 0.000001 ether to pay listing fee"
+        );
+        fairItems[tokenId] = List(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+        nft.transferFrom(msg.sender, address(this), tokenId);
+        emit NftListed(tokenId, msg.sender, address(this), price, false);
+    }
+
+    function buyNft(uint256 tokenId) public payable nonReentrant {
+        uint256 price = fairItems[tokenId].price;
+        require(
+            msg.value == price,
+            "Transfer price amount to complete transaction"
+        );
+        fairItems[tokenId].seller.transfer(msg.value);
+        nft.transferFrom(address(this), msg.sender, tokenId);
+        fairItems[tokenId].sold = true;
+        delete fairItems[tokenId];
+    }
+
+    function cancelSale(uint256 tokenId) public nonReentrant {
+        require(fairItems[tokenId].seller == msg.sender, "NFT not yours");
+        nft.transferFrom(address(this), msg.sender, tokenId);
+        delete fairItems[tokenId];
+    }
+
+    function getPrice(uint256 tokenId) public view returns (uint256) {
+        uint256 price = fairItems[tokenId].price;
+        return price;
+    }
+
+    function nftListings() public view returns (List[] memory) {
+        uint256 nftCount = nft.totalSupply();
+        uint currentIndex = 0;
+        List[] memory items = new List[](nftCount);
+        for (uint i = 0; i < nftCount; i++) {
+            if (fairItems[i + 1].holder == address(this)) {
+                uint currentId = i + 1;
+                List storage currentItem = fairItems[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    function onERC721Received(
+        address,
+        address from,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        require(from == address(0x0), "Cannot send nfts to nftFair directly");
+        return IERC721Receiver.onERC721Received.selector;
     }
 
     function getListingFee() public view returns (uint256) {
