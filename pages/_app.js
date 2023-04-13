f5207af58edd87c2fb1ca0af6402f189f0b989aa
Keen Regen <keenregen@gmail.com>
2023-04-13 05:33:22 +0300

    v3.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,136 @@
-import '@/styles/globals.css'
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import "sf-font";
+import Link from "next/link";
+import {
+  Spacer,
+  Button,
+  Col,
+  Row,
+  Container,
+  Dropdown,
+} from "@nextui-org/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import react from "react";
+import { publicRuntimeConfig } from "@/next.config";
+
+const theme = createTheme({
+  type: "dark",
+  theme: {
+    fontFamily: "SF Pro Display",
+    colors: {
+      primaryLight: "$blue200",
+      primaryLightHover: "$blue300",
+      primaryLightActive: "$blue400",
+      primaryLightContrast: "$blue600",
+      primary: "$purple500",
+      primaryBorder: "$blue500",
+      primaryBorderHover: "$blue600",
+      primarySolidHover: "$blue700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$white500",
+      transparent: "#00000000",
+
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple300 90%)",
+      link: "#5E1DAD",
+
+      myColor: "#00000030",
+    },
+    space: {},
+    fonts: {},
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <div style={{ background: "black" }}>
+        <Container lg css={{ marginTop: "$5" }}>
+          <Row justify="center">
+            <Col css={{ marginTop: "$3" }}>
+              <img
+                style={{
+                  marginLeft: "60px",
+                  width: "180px",
+                  maxHeight: "50px",
+                }}
+                src="geo_3.png"
+              />
+            </Col>
+            <Col css={{ marginTop: "$8" }}>
+              <Link legacyBehavior href="/">
+                <Button
+                  size="sm"
+                  style={{
+                    background: "#00000070",
+                    boxShadow: "0px 0px 4px #ffffff",
+                  }}
+                >
+                  <a
+                    style={{
+                      fontFamily: "SF Pro Display",
+                      fontWeight: "500",
+                      color: "white",
+                      fontSize: "20px",
+                    }}
+                  >
+                    MarketPlace
+                  </a>
+                </Button>
+              </Link>
+            </Col>
+            <Col css={{ marginTop: "$8" }}>
+              <Link legacyBehavior href="/collections">
+                <Button
+                  size="sm"
+                  style={{
+                    background: "#00000070",
+                    boxShadow: "0px 0px 4px #ffffff",
+                  }}
+                >
+                  <a
+                    style={{
+                      fontFamily: "SF Pro Display",
+                      fontWeight: "500",
+                      color: "white",
+                      fontSize: "20px",
+                    }}
+                  >
+                    Collections
+                  </a>
+                </Button>
+              </Link>
+            </Col>
+            <Col css={{ marginTop: "$8" }}>
+              <Button
+                size="sm"
+                style={{
+                  background: "#00000070",
+                  boxShadow: "0px 0px 4px #ffffff",
+                }}
+              >
+                <Link legacyBehavior href="/portal">
+                  <a
+                    style={{
+                      fontFamily: "SF Pro Display",
+                      fontWeight: "500",
+                      color: "white",
+                      fontSize: "20px",
+                    }}
+                  >
+                    My NFT Portal
+                  </a>
+                </Link>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </div>
+    </div>
+  );
 }
+
+export default MyApp;
